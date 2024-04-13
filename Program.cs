85b441352c2f9fb868d1788a4b6022216db114ca
Владимир Толстов <vlatolstov@gmail.com>
2024-04-13 18:09:00 +0400

    start

--- Program.cs
+++ Program.cs
@@ -16,32 +16,13 @@ class Program
 
     public class Solution
     {
+        public class Solution
+        {
+            public int MaximalRectangle(char[][] matrix)
+            {
 
-    }
-
-
-
-}
-public class TreeNode
-{
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-    {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
+            }
+        }
     }
 }
 
