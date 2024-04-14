4009f5f5f10bd6021f95ecb07ab1f0fbaadde8bd
Владимир Толстов <vlatolstov@gmail.com>
2024-04-14 23:41:15 +0400

    result

--- Program.cs
+++ Program.cs
@@ -9,14 +9,46 @@ class Program
     static void Main(string[] args)
     {
         var sol = new Solution();
-
-
-
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        Console.WriteLine(sol.SumOfLeftLeaves(root));
     }
 
     public class Solution
     {
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            int sum = 0;
+            if (root.left != null) sum += LeftLeafSearchAndCount(root.left, true);
+            if (root.right != null)  sum += LeftLeafSearchAndCount(root.right, false);
+            return sum;
+        }
 
+        int LeftLeafSearchAndCount(TreeNode node, bool currIsLeft)
+        {
+            int currSum = 0;
+            var curr = node;
+            switch (curr.left == null, curr.right == null)
+            {
+                case (true, true):
+                    if (currIsLeft) currSum += curr.val;
+                    break;
+                case (true, false):
+                    currSum += LeftLeafSearchAndCount(curr.right, false);
+                    break;
+                case (false, true):
+                    currSum += LeftLeafSearchAndCount(curr.left, true);
+                    break;
+                case (false, false):
+                    currSum += LeftLeafSearchAndCount(curr.left, true);
+                    currSum += LeftLeafSearchAndCount(curr.right, false);
+                    break;
+            };
+            return currSum;
+        }
     }
 
 
