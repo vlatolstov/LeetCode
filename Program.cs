ffda51475a7b650312a089a71829461585cb72c0
Владимир Толстов <vlatolstov@gmail.com>
2024-04-15 15:39:12 +0400

    не работает

--- Program.cs
+++ Program.cs
@@ -10,13 +10,74 @@ class Program
     {
         var sol = new Solution();
 
-
+        Console.WriteLine(sol.FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }));
+        Console.WriteLine(sol.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }));
+        Console.WriteLine(sol.FindMedianSortedArrays(new int[] { 1, 2 }, Array.Empty<int>()));
 
     }
 
     public class Solution
     {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int f = 0;
+            int s = 0;
+            int midInd;
 
+            switch (nums1.Length > 0, nums2.Length > 0)
+            {
+                case (false, false):
+                    return 0d;
+                case (false, true):
+                    midInd = nums2.Length / 2;
+                    if (nums2.Length % 2 == 0) return nums2[midInd];
+                    else return ((double)(nums2[midInd] + nums2[midInd + 1])) / 2;
+                case (true, false):
+                    midInd = nums1.Length / 2;
+                    if (nums1.Length % 2 == 0) return nums1[midInd];
+                    else return ((double)(nums1[midInd] + nums1[midInd + 1])) / 2;
+                case (true, true):
+                    int sumLength = nums1.Length + nums2.Length;
+                    for (int i = 0; i < (sumLength / 2); i++)
+                    {
+                        switch (f < nums1.Length, s < nums2.Length)
+                        {
+                            case (true, true):
+                                if (nums1[f] >= nums2[s]) s++;
+                                else f++;
+                                break;
+                            case (false, true):
+                                s++;
+                                break;
+                            case (true, false):
+                                f++;
+                                break;
+                            default:
+                                throw new Exception("wat");
+                        };
+                    }
+                    if (f >= nums1.Length)
+                    {
+                        return sumLength % 2 == 0 ? ((double)(nums2[s] + nums2[s + 1])) / 2 : (double)nums2[s];
+                    }
+                    if (s >= nums2.Length)
+                    {
+                        return sumLength % 2 == 0 ? ((double)(nums1[f] + nums1[f + 1])) / 2 : (double)nums1[f];
+                    }
+                    else
+                    {
+                        if (sumLength % 2 == 0)
+                        {
+                            return ((double)(nums1[f] + nums2[s])) / 2;
+                        }
+                        else
+                        {
+                            return f > s == true ? nums2[s] : nums1[f];
+                        }
+                    }
+
+            }
+        }
     }
 
 
