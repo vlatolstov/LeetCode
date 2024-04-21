efefe3aba2a2e36e73b32a561d2edb8439eb629c
Владимир Толстов <vlatolstov@gmail.com>
2024-04-21 12:27:15 +0400

    O(n+m)/O(n+m)

--- Program.cs
+++ Program.cs
@@ -9,52 +9,49 @@ class Program
     static void Main(string[] args)
     {
         var sol = new Solution();
-
-
-
+        Console.WriteLine(sol.ValidPath(3, new int[][] { new int[] { 0, 1 }, new int[] { 1,2 }, new int[] { 2,0 } }, 0, 2));
+        //n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+        //n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], source = 0, destination = 5
     }
 
     public class Solution
     {
-
-    }
-
-
-
-}
-public class TreeNode
-{
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-    {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-
-    public static TreeNode CreateTree(int?[] values, int index = 0)
-    {
-        if (index >= values.Length || values[index] == null)
+        public bool ValidPath(int n, int[][] edges, int source, int destination)
         {
-            return null;
-        }
-        TreeNode node = new TreeNode();
-        node.left = CreateTree(values, 2 * index + 1);
-        node.right = CreateTree(values, 2 * index + 2);
-        node.val = (int)values[index];
-        return node;
-    }
-}
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
-}
+            var graph = new Dictionary<int, List<int>>();
 
+            for (int i = 0; i < n; i++)
+            {
+                graph.Add(i, new List<int>());
+            }
+
+            foreach (var edge in edges)
+            {
+                graph[edge[0]].Add(edge[1]);
+                graph[edge[1]].Add(edge[0]);
+            }
+
+            var queue = new Queue<int>();
+            var visited = new HashSet<int>();
+
+            queue.Enqueue(source);
+            visited.Add(source);
+
+            while(queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                if (vertex == destination) return true;
+
+                foreach (var neighbor in graph[vertex])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        queue.Enqueue(neighbor);
+                        visited.Add(neighbor);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
