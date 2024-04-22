97fe052da5346c7744dd63507a06cd876489287c
Владимир Толстов <vlatolstov@gmail.com>
2024-04-23 00:41:30 +0400

    ---

--- Program.cs
+++ Program.cs
@@ -10,51 +10,63 @@ class Program
     {
         var sol = new Solution();
 
+        Console.WriteLine(sol.MinMutation("AAAAAAAA", "CCCCCCCC", new string[] 
+            { 
+                "AAAAAAAA", 
+                "AAAAAAAC", 
+                "AAAAAACC", 
+                "AAAAACCC", 
+                "AAAACCCC", 
+                "AACACCCC", 
+                "ACCACCCC", 
+                "ACCCCCCC", 
+                "CCCCCCCA" }));
 
 
     }
 
     public class Solution
     {
-
-    }
-
-
-
-}
-public class TreeNode
-{
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-    {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-
-    public static TreeNode CreateTree(int?[] values, int index = 0)
-    {
-        if (index >= values.Length || values[index] == null)
+        public int MinMutation(string startGene, string endGene, string[] bank)
         {
-            return null;
-        }
-        TreeNode node = new TreeNode();
-        node.left = CreateTree(values, 2 * index + 1);
-        node.right = CreateTree(values, 2 * index + 2);
-        node.val = (int)values[index];
-        return node;
-    }
-}
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
-}
+            char[] genes = new char[] { 'A', 'T', 'G', 'C' };
+            HashSet<string> valid = new(bank);
+            HashSet<string> visited = new();
+            Queue<string> q = new();
 
+            int steps = 0;
+
+            q.Enqueue(startGene);
+            visited.Add(startGene);
+
+            while (q.Count > 0)
+            {
+                int size = q.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var cur = q.Dequeue();
+                    if (cur == endGene) return steps;
+
+                    for (int j = 0; j < cur.Length; j++)
+                    {
+                        for (int g = 0; g < genes.Length; g++)
+                        {
+                            if (cur[j] == genes[g]) continue;
+                            var curArr = cur.ToCharArray();
+                            curArr[j] = genes[g];
+                            var next = new string(curArr);
+
+                            if (valid.Contains(next) && !visited.Contains(next))
+                            {
+                                q.Enqueue(next);
+                                visited.Add(next);
+                            }
+                        }
+                    }
+                }
+                steps++;
+            }
+            return -1;
+        }
+    }
+}
