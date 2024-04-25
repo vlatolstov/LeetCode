d6b0ba7864f7a756b413098035c3ea106ca5ac5c
Владимир Толстов <vlatolstov@gmail.com>
2024-04-25 14:16:05 +0400

    -

--- Program.cs
+++ Program.cs
@@ -10,8 +10,9 @@ class Program
     {
         var sol = new Solution();
 
-        Console.WriteLine(sol.LongestIdealString("acfgbd", 2));
-        Console.WriteLine(sol.LongestIdealString("abcd", 3));
+        //Console.WriteLine(sol.LongestIdealString("acfgbd", 2));
+        //Console.WriteLine(sol.LongestIdealString("abcd", 3));
+        Console.WriteLine(sol.LongestIdealString("abfgkajsnsdfnwwwkdjksmdjkrnksmdnfsdhertfddgdjhrkjeretfehqwsdfjhtyjwedfsdmfm", 7));
     }
 
     public class Solution
@@ -19,43 +20,42 @@ class Program
         public int LongestIdealString(string s, int k)
         {
             if (s.Length == 1) return 1;
-            HashSet<string> visited = new();
-
-            Queue<(int, string)> q = new(); //int for current position in 's', string for current string
 
             
+            Queue<string> q = new();
 
-            while (q.Count > 0)
+            var first = s[0].ToString();
+            q.Enqueue(first);
+
+            for (int i = 1; i < s.Length; i++)
             {
+                var nextChar = s[i];
+
                 int size = q.Count;
-                for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
                 {
                     var cur = q.Dequeue();
-                    var curPos = cur.Item1;
-                    var curString = cur.Item2;
-                    if (curPos < s.Length - 1)
+
+                    var difference = Math.Abs(cur[^1] - nextChar);
+                    if (difference <= k)
                     {
-                        var nextPos = curPos + 1;
-                        var nextChar = s[nextPos];
-                        var difference = Math.Abs(curString[^1] - nextChar);
-                        if (difference <= k)
-                        {
-                            var newString = new string(curString + nextChar);
-                            if (!visited.Contains(newString))
-                            {
-                                visited.Add(newString);
-                                q.Enqueue((nextPos, newString));
-                            }
-                        }
+                        var newString = new string(cur + nextChar);
+                        q.Enqueue(newString);
+                    }
+                    else
+                    {
+                        q.Enqueue(cur);
+                        q.Enqueue(s[i].ToString());
                     }
                 }
             }
-
             int longest = 0;
-            foreach (var seq in visited)
+            while(q.Count > 0)
             {
-                longest = longest > seq.Length ? longest : seq.Length;
+                var lenght = q.Dequeue().Length;
+                longest = longest > lenght ? longest : lenght;
             }
+            
             return longest;
         }
     }
